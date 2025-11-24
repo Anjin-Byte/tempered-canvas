89a4b9d70d1c6efc96566667dc2b36630dc3b4d4
Anjin-Byte <hale.taylor.dev@gmail.com>
2025-11-23 20:37:22 -0600

    Initial commit

---
+++ tempered-canvas/tempered-canvas/tempered_canvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  tempered_canvasApp.swift
+//  tempered-canvas
+//
+//  Created by Taylor Hale on 11/23/25.
+//
+
+import SwiftUI
+
+@main
+struct tempered_canvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tempered-canvas/tempered-canvasTests/tempered_canvasTests.swift
@@ -0,0 +1,17 @@
+//
+//  tempered_canvasTests.swift
+//  tempered-canvasTests
+//
+//  Created by Taylor Hale on 11/23/25.
+//
+
+import Testing
+@testable import tempered_canvas
+
+struct tempered_canvasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
